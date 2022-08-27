d5e068811e5d8697554af24bb340bcca14571320
shafiquedhuyo <shafiquerehman7@gmail.com>
2022-08-27 15:16:37 +0300

    Attributes

--- OOP/ClassesAndObject/ClassesAndObject/src/com/dhuyo/study/Main.java
+++ OOP/ClassesAndObject/ClassesAndObject/src/com/dhuyo/study/Main.java
@@ -3,10 +3,12 @@ package com.dhuyo.study;
 public class Main {
 	
 	int x = 5;
+	String name = "Juman"; // Attribute or property or field of class Main
 
 	public static void main(String[] args) {
 		Main ob = new Main();
 		System.out.println(ob.x);
+		System.out.println(ob.name);
      
 	}
 
---
+++ OOP/ClassesAndObject/ClassesAndObject/src/com/dhuyo/study/attributes/Student.java
@@ -0,0 +1,16 @@
+package com.dhuyo.study.attributes;
+
+public class Student {
+	
+	// Below are student attributes or properties for fields
+	String name = "Juman";
+	String fatherName = "Abdul Rehman";
+	String rollNUmber = "0221FBSE064";
+	String universityName = "SSEUED";
+	
+  //Below main method is entry point for our program	
+  public static void main(String[] args) {
+	  
+  }	
+
+}
