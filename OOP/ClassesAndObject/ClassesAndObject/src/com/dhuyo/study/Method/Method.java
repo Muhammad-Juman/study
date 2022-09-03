367651892108730599efb544cc6327a155c004f6
Muhammad Juman <108765580+Muhammad-Juman@users.noreply.github.com>
2022-09-03 05:08:03 -0700

    Sitting Programs

---
+++ OOP/ClassesAndObject/ClassesAndObject/src/com/dhuyo/study/Method/Method.java
@@ -0,0 +1,15 @@
+package com.dhuyo.study.attributes;
+
+public class Method {
+	private int a=5;
+	public void method1(int b) {
+		
+	}
+public static void main(String[] args) {
+    Method object=new Method();
+    object.method1(5);
+    
+    		
+    		
+}
+}
