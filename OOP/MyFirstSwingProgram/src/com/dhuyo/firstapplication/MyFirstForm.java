1d430bc902e9a87a2e3b43b5dac60efc796fe55f
shafiquedhuyo <shafiquerehman7@gmail.com>
2022-11-03 00:32:49 +0300

    added swing project

---
+++ OOP/MyFirstSwingProgram/src/com/dhuyo/firstapplication/MyFirstForm.java
@@ -0,0 +1,21 @@
+package com.dhuyo.firstapplication;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class MyFirstForm extends JFrame{
+	
+	
+	
+	public MyFirstForm() {
+		setVisible(true);
+	}
+
+	
+	
+	
+	public static void main(String[] args) {
+		MyFirstForm ob = new MyFirstForm();
+	}
+
+}
